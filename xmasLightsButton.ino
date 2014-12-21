f5bc46d14f1061392dde09651eac558dee8a95f7
Davey <djt64r@gmail.com>
2014-12-20 20:09:12 -0500

    Update xmasLightsButton.ino

--- xmasLightsButton.ino
+++ xmasLightsButton.ino
@@ -1,4 +1,19 @@
+//Created by Davey Toney
+//on 12/20/14
 
+//Here is some code I wrote as well as adapted to be used on the Arduino to control Christmas lights.  
+The user can set the desired number of hours the outlet is on via a push button.  
+This code could be expanded upon to be used with a 7-segment display which would show the number of 
+hours the user has entered, without the use of the serial monitor.  //
+
+//I used this code on an Arduino Uno which was part of a basic relay circuit (the specifics of which 
+can be found in various locations online) that controlled two sets of outdoor christmas lights--this
+involved the use of high-voltage household AC which can be dangerous, but code that follows can't hurt
+you.//
+
+//Works Cited: 
+//1.  Debounce function code: Jeremy Blum. http://www.jeremyblum.com/2011/01/10/arduino-tutorial-2-now-with-more-blinky-things/
+//
 
 const int hourPin = 8; //location of hour increment button on arduino.//
 const int startButton = 12; //location of start button that starts the timer loop.//
